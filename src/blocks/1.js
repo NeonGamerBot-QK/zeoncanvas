dea349ae50d63d4e5965bfcfc8be3af6076aa8ed
Saahil <neon@saahild.com>
2023-11-01 14:11:31 -0400

    Update 1.js

--- src/blocks/1.js
+++ src/blocks/1.js
@@ -1,6 +1,10 @@
+//WEBEDITOROVERRIDE
+//meta:{"createdStamp":1698862271009,"color":"#ff0000","webui":true,"v":1}
+// the line above should not be removed in any case
+// that line specifies that it was from the WebUI
+// this code was written from the WEBuI Editor
 module.exports = (ctx) => {
-    // wow is this an empty file space>
-    // you should add some code here
-    // or even a PR (wowie)
-
+        ctx.fillStyle = "#ff0000"
+        ctx.rect(0, 0, 64,64)
+        ctx.fill()
 }

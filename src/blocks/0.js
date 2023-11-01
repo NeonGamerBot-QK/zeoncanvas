8108b79590f707899abefafee54f50fdb008f74f
Saahil <neon@saahild.com>
2023-11-01 14:06:34 -0400

    Update 0.js

--- src/blocks/0.js
+++ src/blocks/0.js
@@ -1,6 +1,4 @@
 module.exports = (ctx) => {
-    // wow is this an empty file space>
-    // you should add some code here
-    // or even a PR (wowie)
-// wow i added such e epic code
+    ctx.fillStyle = 'red'
+    ctx.fill()
 }

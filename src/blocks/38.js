99f17eaafcf10ee72b32b2b6fbaf9147973de5e5
Saahil <neon@saahild.com>
2024-09-03 09:42:56 -0400

    Update 38.js

--- src/blocks/38.js
+++ src/blocks/38.js
@@ -2,5 +2,7 @@ module.exports = (ctx) => {
     // wow is this an empty file space>
     // you should add some code here
     // or even a PR (wowie)
-
+  ctx.fillStyle = '#bedabe'
+  ctx.rect(0, 0, 64, 64)
+  ctx.fill()
 }
